a761127adaa085dfc478a19cfff9f81de4a85e21
Andhika Dwi Maulana <andhkdwmln@gmail.com>
2024-10-16 12:09:40 -0700

    555 !

--- run.js
+++ run.js
@@ -3,6 +3,7 @@ const bot = new Dumper();
 
 const fetch = require('node-fetch');
 const fs = require('fs');
+const chalk = require('chalk');
 
 (async () => {
 
@@ -23,61 +24,13 @@ const fs = require('fs');
             .then(res => {
                 res.body.pipe(fs.createWriteStream(`./data/${filename}.jpg`))
             })
-            console.log(`[+] Downloading => ${filename}.jpg`);
+            console.log(chalk.green(`[+] Downloading => ${filename}.jpg`));
             
         } else {
-            console.log(`[-] Skipping => ${filename}.jpg`);
+            console.log(chalk.red(`[-] Skipping => ${filename}.jpg`));
             
         }
 
     }
 
 })();
-
-// const fetch = require('node-fetch');
-// const fs = require('fs');
-
-// (async () => {
-
-//     if(!fs.existsSync('./result')) fs.mkdirSync('./result');
-
-//     const data = fs.readFileSync('./data/data.json');
-//     const json = JSON.parse(data);
-
-//     for (let i = 0; i < json.length; i++) {
-        
-//         const facultyName = json[i].faculty;
-//         const angkatan = json[i].data.angkatan;
-//         const ingfo = json[i].data.info;
-
-//         for (let j = 0; j < ingfo.length; j++) {
-            
-//             const name_mentah = ingfo[j].name;
-//             const naming = name_mentah.replace(/\s/g, '_');
-//             const name = naming.replace(/[^a-zA-Z_]/g, '');
-//             const nim = ingfo[j].nim;
-//             const image = ingfo[j].img;
-
-//             if(!fs.existsSync(`./result/${facultyName}`)) fs.mkdirSync(`./result/${facultyName}`);
-//             if(!fs.existsSync(`./result/${facultyName}/${angkatan}`)) fs.mkdirSync(`./result/${facultyName}/${angkatan}`);
-
-//             const req = await fetch(image);
-//             if(req.status === 200) {
-//                 await fetch(image)
-//                 .then(res =>
-//                     res.body.pipe(fs.createWriteStream(`./result/${facultyName}/${angkatan}/${name}_${nim}.jpg`))
-//                 );
-
-//                 console.log(`[+] Downloading ${facultyName} ${angkatan} => ${image}`);
-
-//             } else {
-
-//                 console.log(`[-] Skipping ${facultyName} ${angkatan} => ${image}`);
-
-//             }
-
-//         }
-
-//     }
-
-// })();
